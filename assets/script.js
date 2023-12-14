583fa5e67563cf3fba6cad35a154e1ac54f68e4c
Ben <144111469+bengilbertcodes@users.noreply.github.com>
2023-12-14 11:44:53 +0000

    Add media queries and JS for document height

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,10 @@
+const documentHeight = () => {
+    const doc = document.documentElement
+    doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+   }
+   window.addEventListener('resize', documentHeight)
+   documentHeight()
+
 var count = 0;
 
 // animate the action boxes
